946ad1db7c880ed3131edfc0a074cae4deb0ad95
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-07-31 15:23:27 -0300

    fix port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_CONNECT, {
   useUnifiedTopology: true
 
 }).then(() => {
-  app.listen($PORT || 3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log('server start');
   });
 }).catch((err) => {
